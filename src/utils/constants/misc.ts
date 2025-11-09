d477317a9dbd13493301f58bbc87cd5c7102b690
Bryan0502 <dashteinmain@gmail.com>
2025-11-09 17:14:13 -0500

    update companies

--- src/utils/constants/misc.ts
+++ src/utils/constants/misc.ts
@@ -6,29 +6,13 @@ export const PAGINATION_LIMIT = 10;
 
 export const COMPANIES = [
     {
-        name: "Asana",
+        name: "Malek Seguros",
         logo: "/assets/company-01.svg",
     },
     {
-        name: "Tidal",
+        name: "Planticería",
         logo: "/assets/company-02.svg",
     },
-    {
-        name: "Innovaccer",
-        logo: "/assets/company-03.svg",
-    },
-    {
-        name: "Linear",
-        logo: "/assets/company-04.svg",
-    },
-    {
-        name: "Raycast",
-        logo: "/assets/company-05.svg",
-    },
-    {
-        name: "Labelbox",
-        logo: "/assets/company-06.svg",
-    }
 ] as const;
 
 export const PROCESS = [
