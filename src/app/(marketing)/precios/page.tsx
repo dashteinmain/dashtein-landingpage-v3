34107b6e9a904c05bbfd6ba19608f6401e6bf98b
Bryan0502 <dashteinmain@gmail.com>
2025-11-22 20:57:02 -0500

    Update Faqs

--- src/app/(marketing)/precios/page.tsx
+++ src/app/(marketing)/precios/page.tsx
@@ -37,10 +37,10 @@ const PricingPage = () => {
                 <div className="mt-20 w-full">
                     <div className="flex flex-col items-center justify-center w-full pt-12">
                         <h2 className="mt-6 text-2xl font-semibold text-center lg:text-3xl xl:text-4xl">
-                            Frequently Asked Questions
+                            Preguntas Frecuentes
                         </h2>
                         <p className="max-w-lg mt-6 text-center text-neutral-500">
-                            Here are some of the most common questions we get asked. If you have a question that isn&apos;t answered here, feel free to reach out to us.
+                            Aquí hay algunas de las consultas que más nos hacen. Si tu consulta no aparece, contáctanos.
                         </p>
                     </div>
                     <div className="max-w-3xl mx-auto w-full mt-20">
